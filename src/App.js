035aa3ae76ade22865cfec31e4443a77ffea2163
anton41k <anton12petrenko@gmail.com>
2021-09-15 10:42:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-//import { Switch, Route, Link } from 'react-router-dom';
-//import TodosView from './views/TodosView';
+
 import { ToastContainer } from "react-toastify";
 import GalleryView from "./view/GalleryView";
 
