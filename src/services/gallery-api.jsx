f9cf45ab4e010219e5fbfd2b7185e0e404cd35fe
anton41k <anton12petrenko@gmail.com>
2021-09-17 10:55:48 +0300

    Update gallery-api.jsx

--- src/services/gallery-api.jsx
+++ src/services/gallery-api.jsx
@@ -1,9 +1,10 @@
+const URL = "https://pixabay.com/api/";
+
 const fetchGallery = ({ searchQuery = "", currentPage = 1, pageSize = 12 }) => {
-  const url = "https://pixabay.com/api/";
   const key = "22580473-9722fdac11ed5197610aea928";
   const options = "image_type=photo&orientation=horizontal";
-  const BASE_URL = `${url}?q=${searchQuery.trim()}&page=${currentPage}&key=${key}&${options}&per_page=${pageSize}`;
-  return fetch(BASE_URL).then((response) => response.json());
+  const baseUrl = `${URL}?q=${searchQuery.trim()}&page=${currentPage}&key=${key}&${options}&per_page=${pageSize}`;
+  return fetch(baseUrl).then((response) => response.json());
 };
 
 export default { fetchGallery };
